75d54eadfe709c4777967973af71c80f2a6272b3
Gabriel-Weslley <gabriel.bonfim@novaandradina.org>
2020-04-27 02:54:39 -0400

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'pgadmin',
+  password: 'pgadmin',
+  database: 'sqlnode',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
